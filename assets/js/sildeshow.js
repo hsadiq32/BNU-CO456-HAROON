04efe60e5d85f3c7c5bef9f6c3478ac62cf51590
hsadiq32 <20135112s@gmail.com>
2021-01-14 11:32:53 +0000

    Added slideshpw with some sample images with gully customised look

---
+++ assets/js/sildeshow.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    // Initializing the swiper plugin for the slider.
+    
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    });
+    
+});
