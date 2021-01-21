2f6e312671046dd0bfb84af8ea32fcf8c57fa9b0
hsadiq32 <20135112s@gmail.com>
2021-01-21 15:14:14 +0000

    added more animations and comments

--- assets/js/homepage_slider.js
+++ assets/js/homepage_slider.js
@@ -3,11 +3,11 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var x = document.getElementsByClassName("mySlides"); // Finds the images with the specified class name
+  for (i = 0; i < x.length; i++) { // Finds length of total images
+    x[i].style.display = "none"; // Hides images
   }
-  slideIndex++;
+  slideIndex++; // Keeps track of the slide number
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 375); // Change image every 0.375 seconds
